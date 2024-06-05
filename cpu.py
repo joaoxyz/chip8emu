e6f70cdbb56656561753ebdcc76872273a2b08b9
joaoxyz <jpreis808@gmail.com>
2024-06-04 21:55:10 -0300

    Finish draw instruction (IBM Logo rom works :D)

--- cpu.py
+++ cpu.py
@@ -26,7 +26,7 @@ class CPU:
         third_nibble = instruction.hex()[2]
         fourth_nibble = instruction.hex()[3]
 
-        second_byte = instruction.hex()[1:3]
+        second_byte = instruction.hex()[2:]
         nibbles234 = instruction.hex()[1:]
 
         match first_nibble:
@@ -40,40 +40,40 @@ class CPU:
                         self.program_counter = self.stack.pop()
             case '1':
                 # 1NNN: Jump
-                self.program_counter = int(nibbles234, 16)
+                self.program_counter = int(nibbles234, base=16)
             case '6':
                 # 6XNN: Set
-                self.variable_registers[int(second_nibble, 16)] = int(second_byte, 16)
+                self.variable_registers[int(second_nibble, base=16)] = int(second_byte, base=16)
             case '7':
                 # 7XNN: Add
-                self.variable_registers[int(second_nibble, 16)] += int(second_byte, 16)
+                self.variable_registers[int(second_nibble, base=16)] += int(second_byte, base=16)
                 # Wrap-around on overflow
-                self.variable_registers[int(second_nibble, 16)] &= 255
+                self.variable_registers[int(second_nibble, base=16)] &= 255
             case 'a':
                 # ANNN: Set index register
-                self.index_register = int(nibbles234, 16)
+                self.index_register = int(nibbles234, base=16)
             case 'd':
                 # DXYN: Display draw
-                x_cord = self.variable_registers[int(second_nibble, 16)] & 63
-                y_cord = self.variable_registers[int(third_nibble, 16)] & 63
-                sprite_memory_pos = self.index_register
-                sprite_height = int(fourth_nibble, 16)
-                sprite_width = 8
+                x_cord = self.variable_registers[int(second_nibble, base=16)] & 63
+                y_cord = self.variable_registers[int(third_nibble, base=16)] & 31
                 self.variable_registers[-1] = 0
+                sprite_height = int(fourth_nibble, base=16)
+                sprite_width = 8
 
-                for y in range(y_cord, min(len(self.display), sprite_height)):
-                    sprite = format(bin(self.memory[sprite_memory_pos]), 'b')
+                sprite_memory_pos = self.index_register
+                for y in range(y_cord, min(len(self.display), y_cord+sprite_height)):
+                    sprite = f'{int(self.memory[sprite_memory_pos].hex(), base=16):08b}'
                     sprite_idx = 0
-                    for x in range(x_cord, min(len(self.display[0]), sprite_width)):
+                    for x in range(x_cord, min(len(self.display[0]), x_cord+sprite_width)):
+                        self.display[y][x] ^= int(sprite[sprite_idx], base=2)
                         # Update register VF on pixel turnoff
-                        if (self.display[y][x] == 1 and int(sprite[sprite_idx])):
+                        if (self.display[y][x] == 1 and int(sprite[sprite_idx], base=2)):
                             self.variable_registers[-1] = 1
-                        self.display[y][x] ^= int(sprite[sprite_idx])
                         sprite_idx += 1
                     sprite_memory_pos += 1
 
     def dump_display(self):
-        for i in self.display:
-            for j in i:
-                print(j, end='')
+        for col in range(len(self.display)):
+            for row in range(len(self.display[0])):
+                print(self.display[col][row], end='')
             print()
--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import cpu
+import time
 import sys
-import sdl2, sdl2.ext
-
-def run():
-    sdl2.ext.init()
-    window = sdl2.ext.Window("The Pong Game", size=(64, 32))
-    window.show()
-    running = True
-    while running:
-        events = sdl2.ext.get_events()
-        for event in events:
-            if event.type == sdl2.SDL_QUIT:
-                running = False
-                break
-        window.refresh()
-    return 0
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -29,10 +15,11 @@ if __name__ == "__main__":
             chip8cpu.memory[read_index] = byte
             read_index += 1
 
-    '''
-    for i in range():
-        inst = chip8cpu.fetch()
-        chip8cpu.decode_and_execute(inst)
-    '''
+    try:
+        while True:
+            inst = chip8cpu.fetch()
+            chip8cpu.decode_and_execute(inst)
+    except KeyboardInterrupt:
+        chip8cpu.dump_display()
 
-    sys.exit(run())
+    #sys.exit(run(chip8cpu))
