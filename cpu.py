7ba7c08018094f4c9978d96b2826d3364996ee6c
joaoxyz <jpreis808@gmail.com>
2025-08-19 09:44:33 -0300

    Refactor instruction fetching and decoding

--- cpu.py
+++ cpu.py
@@ -13,6 +13,7 @@ class CPU:
         self.index_register: int = 0
         self.stack: list[int] = []
         # Timers initialized as zero
+        # TODO: Research how timers work??
         self.delay_timer = 0
         self.sound_timer = 0
         self.variable_registers: list[int] = [0] * 16
@@ -44,130 +45,145 @@ class CPU:
         positions = list(range(0x50, 0x50+len(font_arr), 5))
         self.font_positions = dict(zip(characters, positions))
 
-        # TODO: Research how timers work??
-
-    def fetch(self) -> bytes:
-        instruction = self.memory[self.program_counter].to_bytes() + self.memory[self.program_counter+1].to_bytes()
+    def fetch(self) -> int:
+        byte1 = self.memory[self.program_counter]
+        byte2 = self.memory[self.program_counter+1]
+        instruction = (byte1 << 8) | byte2
         self.program_counter += 2
         return instruction
 
-    def decode_and_execute(self, instruction: bytes) -> None:
-        nibble1 = instruction.hex()[0]
-        nibble2 = instruction.hex()[1]
-        nibble3 = instruction.hex()[2]
-        nibble4 = instruction.hex()[3]
+    def decode_and_execute(self, instruction: int) -> None:
+        nibble1 = (instruction & 0xF000) >> 12
+        nibble2 = (instruction & 0x0F00) >> 8
+        nibble3 = (instruction & 0x00F0) >> 4
+        nibble4 = (instruction & 0x000F)
 
-        byte2 = instruction.hex()[2:]
-        nibbles234 = instruction.hex()[1:]
+        x = nibble2
+        y = nibble3
+        n = nibble4
+        nn = instruction & 0x00FF
+        nnn = instruction & 0x0FFF
 
         match nibble1:
-            case '0':
-                match byte2:
-                    case 'e0':
+            case 0x0:
+                match nn:
+                    case 0xE0:
                         # 00E0: Clear screen
-                        self.display[self.display != 0] = 0
-                    case 'ee':
+                        self.display.fill(0)
+                    case 0xEE:
                         # 00EE: Return subroutine
                         self.program_counter = self.stack.pop()
                     case _:
-                        print(f'Unknown instruction: {instruction.hex()}')
-            case '1':
+                        print(f'Unknown instruction: {hex(instruction)}')
+            case 0x1:
                 # 1NNN: Jump
-                self.program_counter = int(nibbles234, base=16)
-            case '2':
+                self.program_counter = nnn
+            case 0x2:
                 # 2NNN: Call subroutine
                 self.stack.append(self.program_counter)
-                self.program_counter = int(nibbles234, base=16)
-            case '3':
+                self.program_counter = nnn
+            case 0x3:
                 # 3XNN: Skip if VX == NN
-                if self.variable_registers[int(nibble2, base=16)] == int(byte2, base=16):
+                if self.variable_registers[x] == nn:
                     self.program_counter += 2
-            case '4':
+            case 0x4:
                 # 4XNN: Skip if VX != NN
-                if self.variable_registers[int(nibble2, base=16)] != int(byte2, base=16):
+                if self.variable_registers[x] != nn:
                     self.program_counter += 2
-            case '5':
+            case 0x5:
                 # 5XY0: Skip if VX == VY
-                if self.variable_registers[int(nibble2, base=16)] == self.variable_registers[int(nibble3, base=16)]:
+                if self.variable_registers[x] == self.variable_registers[y]:
                     self.program_counter += 2
-            case '6':
+            case 0x6:
                 # 6XNN: Set
-                self.variable_registers[int(nibble2, base=16)] = int(byte2, base=16)
-            case '7':
+                self.variable_registers[x] = nn
+            case 0x7:
                 # 7XNN: Add
-                self.variable_registers[int(nibble2, base=16)] += int(byte2, base=16)
+                self.variable_registers[x] += nn
                 # Wrap-around on overflow
-                self.variable_registers[int(nibble2, base=16)] &= 255
-            case '8':
+                self.variable_registers[x] &= 255
+            case 0x8:
+                # Logical and arithmetic instructions
                 match nibble4:
-                    case '0':
-                        self.variable_registers[int(nibble2, base=16)] = self.variable_registers[int(nibble3, base=16)]
-                    case '1':
-                        self.variable_registers[int(nibble2, base=16)] |= self.variable_registers[int(nibble3, base=16)]
-                    case '2':
-                        self.variable_registers[int(nibble2, base=16)] &= self.variable_registers[int(nibble3, base=16)]
-                    case '3':
-                        self.variable_registers[int(nibble2, base=16)] ^= self.variable_registers[int(nibble3, base=16)]
-                    case '4':
-                        self.variable_registers[int(nibble2, base=16)] += self.variable_registers[int(nibble3, base=16)]
+                    case 0x0:
+                        # 8XY0: Set
+                        self.variable_registers[x] = self.variable_registers[y]
+                    case 0x1:
+                        # 8XY1: Binary OR
+                        self.variable_registers[x] |= self.variable_registers[y]
+                    case 0x2:
+                        # 8XY2: Binary AND
+                        self.variable_registers[x] &= self.variable_registers[y]
+                    case 0x3:
+                        # 8XY3: Bitwise XOR
+                        self.variable_registers[x] ^= self.variable_registers[y]
+                    case 0x4:
+                        # 8XY4: Add
+                        self.variable_registers[x] += self.variable_registers[y]
                         # Wrap-around on overflow and set carry flag
-                        if self.variable_registers[int(nibble2, base=16)] > 255:
-                            self.variable_registers[int(nibble2, base=16)] &= 255
+                        if self.variable_registers[x] > 255:
+                            self.variable_registers[x] &= 255
                             self.variable_registers[-1] = 1
                         else:
                             self.variable_registers[-1] = 0
-                    case '5':
-                        vx = self.variable_registers[int(nibble2, base=16)]
-                        vy = self.variable_registers[int(nibble3, base=16)]
+                    case 0x5:
+                        # 8XY5: Subtract VX - VY
+                        vx = self.variable_registers[x]
+                        vy = self.variable_registers[y]
                         # Set carry flag
                         if vx < vy:
                             self.variable_registers[-1] = 0
                         else:
                             self.variable_registers[-1] = 1
                         # x - y
-                        self.variable_registers[int(nibble2, base=16)] = (vx - vy) & 255
-                    case '7':
-                        vx = self.variable_registers[int(nibble2, base=16)]
-                        vy = self.variable_registers[int(nibble3, base=16)]
+                        self.variable_registers[x] = (vx - vy) & 255
+                    case 0x7:
+                        # 8XY7: Subtract VY - VX
+                        vx = self.variable_registers[x]
+                        vy = self.variable_registers[y]
                         # Set carry flag
                         if vy < vx:
                             self.variable_registers[-1] = 0
                         else:
                             self.variable_registers[-1] = 1
                         # x - y
-                        self.variable_registers[int(nibble2, base=16)] = (vy - vx) & 255
-                    case '6':
+                        self.variable_registers[x] = (vy - vx) & 255
+                    case 0x6:
+                        # 8XY6: Shift right
                         # TODO: Ambiguous instruction, behavior should be configurable
                         # Set VF to bit shifted out
-                        self.variable_registers[-1] = self.variable_registers[int(nibble2, base=16)] & 1
-                        self.variable_registers[int(nibble2, base=16)] >>= 1
-                    case 'e':
+                        self.variable_registers[-1] = self.variable_registers[x] & 1
+                        self.variable_registers[x] >>= 1
+                    case 0xE:
+                        # 8XY6: Shift left
                         # TODO: Ambiguous instruction, behavior should be configurable
                         # Set VF to bit shifted out
-                        self.variable_registers[-1] = (self.variable_registers[int(nibble2, base=16)] & 128) >> 7
-                        self.variable_registers[int(nibble2, base=16)] <<= 1
+                        self.variable_registers[-1] = (self.variable_registers[x] & 128) >> 7
+                        self.variable_registers[x] <<= 1
                         # Handle overflow
-                        self.variable_registers[int(nibble2, base=16)] &= 0xFF
+                        self.variable_registers[x] &= 0xFF
                     case _:
-                        print(f'Unknown instruction: {instruction.hex()}')
-            case '9':
+                        print(f'Unknown instruction: {hex(instruction)}')
+            case 0x9:
                 # 9XY0: Skip if VX != VY
-                if self.variable_registers[int(nibble2, base=16)] != self.variable_registers[int(nibble3, base=16)] :
+                if self.variable_registers[x] != self.variable_registers[y] :
                     self.program_counter += 2
-            case 'a':
+            case 0xA:
                 # ANNN: Set index register
-                self.index_register = int(nibbles234, base=16)
-            case 'b':
+                self.index_register = nnn
+            case 0xB:
+                # BNNN: Jump with offset
                 # TODO: Ambiguous instruction, could be made configurable
-                self.program_counter = int(nibbles234, base=16) + self.variable_registers[0]
-            case 'c':
-                self.variable_registers[int(nibble2, base=16)] = randint(0, sys.maxsize) & int(byte2, base=16)
-            case 'd':
+                self.program_counter = nnn + self.variable_registers[0]
+            case 0xC:
+                # CXNN: Random number generation
+                self.variable_registers[x] = randint(0, 255) & nn
+            case 0xD:
                 # DXYN: Draw to display
-                x_cord = self.variable_registers[int(nibble2, base=16)] & 63
-                y_cord = self.variable_registers[int(nibble3, base=16)] & 31
+                x_cord = self.variable_registers[x] & 63
+                y_cord = self.variable_registers[y] & 31
                 self.variable_registers[-1] = 0
-                sprite_height = int(nibble4, base=16)
+                sprite_height = n
                 sprite_width = 8
 
                 sprite_memory_pos = self.index_register
@@ -182,42 +198,46 @@ class CPU:
                             self.variable_registers[-1] = 1
                         sprite_idx += 1
                     sprite_memory_pos += 1
-            case 'f':
-                match byte2:
-                    case '1e':
-                        self.index_register += self.variable_registers[int(nibble2, base=16)]
+            case 0xF:
+                match nn:
+                    case 0x1E:
+                        # FX1E: Add to index
+                        self.index_register += self.variable_registers[x]
                         # Handle overflow
                         if self.index_register > 0x0FFF:
                             self.variable_registers[-1] = 1
                         if self.index_register > 0xFFFF:
                             self.index_register &= 0xFFFF
-                    #case '0a':
-                    case '29':
+                    #case 0x0A:
+                    case 0x29:
                         # FX29: Set index to font data for character in VX
-                        char = self.variable_registers[int(nibble2, base=16)].to_bytes().hex()[1]
+                        char = self.variable_registers[x].to_bytes().hex()[1]
                         self.index_register = self.font_positions[char]
-                    case '33':
-                        num = self.variable_registers[int(nibble2, base=16)]
+                    case 0x33:
+                        # FX33: Binary-coded decimal conversion
+                        num = self.variable_registers[x]
                         i = 2
                         while i >= 0:
                             digit = num % 10
                             self.memory[self.index_register+i] = digit
                             num //= 10
                             i -= 1
-                    case '55':
+                    case 0x55:
+                        # FX55: Store
                         # Ambiguous instruction!
-                        upper_bound = int(nibble2, base=16)
+                        upper_bound = x
                         for i in range(upper_bound+1):
                             self.memory[self.index_register+i] = self.variable_registers[i]
-                    case '65':
+                    case 0x65:
+                        # FX65: Load
                         # Ambiguous instruction!
-                        upper_bound = int(nibble2, base=16)
+                        upper_bound = x
                         for i in range(upper_bound+1):
                             self.variable_registers[i] = self.memory[self.index_register+i]
                     case _:
-                        print(f'Unknown instruction: {instruction.hex()}')
+                        print(f'Unknown instruction: {hex(instruction)}')
             case _:
-               print(f'Unknown instruction: {instruction.hex()}')
+               print(f'Unknown instruction: {hex(instruction)}')
 
     def dump_display(self) -> None:
         for col in range(len(self.display)):
